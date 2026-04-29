650a090ee1782fb9c50c8222dd507d696aa95b6c
Kenith Leon <162732766+kenithleon@users.noreply.github.com>
2026-04-29 21:27:08 +0530

    fix blank page on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add this line to include .glb files (and potentially .png if not already handled)
-  assetsInclude: ['**/*.glb', '**/*.png'], // <--- ADD OR MODIFY THIS LINE
+  base: "/",   // 👈 add this
+  assetsInclude: ['**/*.glb', '**/*.png'],
 })
